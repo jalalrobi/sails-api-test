394ef79dd6f1b51837f9f975da775edd41826f51
Robin <robin@dexsa.org>
2016-02-07 15:55:56 +0600

    initial commit

---
+++ api/models/Test.js
@@ -0,0 +1,27 @@
+/**
+* Test.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    id:{
+      type: 'integer',
+      autoIncrement: true
+    },
+    name:{
+      type: 'string',
+      required: true
+    },
+    lat:{
+      type: 'string'
+    },
+    long:{
+      type: 'string'
+    }
+  }
+};
+
